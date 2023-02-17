6482a5110685fcdcd4d25361d0be41d414da3a05
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-02-18 02:08:24 +0530

    first Commit

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+
+const Homepage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Work sans"
+          color="black"
+          textAlign="center"
+          fontWeight={300}
+        >
+          Chat-ER-Box
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
