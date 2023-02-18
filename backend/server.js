f8b6261a24a049e7119e5d0603dda548e28fa4ee
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-02-18 17:02:41 +0530

    MongoDB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+
+connectDB();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running successfully");
@@ -21,4 +25,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT ${5000}`));
+app.listen(PORT, console.log(`Server started on PORT ${5000}`.yellow.bold));
